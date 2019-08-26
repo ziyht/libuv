bdf51125a1d6d4b8e4151507e669dcea3c4fe72c
ziyht <joyhaitao@foxmail.com>
2019-08-27 00:55:19 +0800

    fix build on windows for codec problems

--- test/test-idna.c
+++ test/test-idna.c
@@ -1,4 +1,4 @@
-/* Copyright The libuv project and contributors. All rights reserved.
+﻿/* Copyright The libuv project and contributors. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
